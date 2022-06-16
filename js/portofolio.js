34ad8294ff392fe48db8b1eff953e3c88722a66d
Wukualam22 <wukualam22@gmail.com>
2022-06-16 16:03:25 +0700

    Update CSS Mobile

---
+++ js/portofolio.js
@@ -0,0 +1,42 @@
+const toggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
+
+let isopen = true;
+toggle.addEventListener('click', () => {
+    if (isopen) {
+        nav.animate([
+            {
+                top: '0',
+                opacity: '0'
+            },
+            {
+                top: '59px',
+                opacity: '1'
+            },
+        ], {
+            duration: 300,
+            fill: 'forwards'
+        });
+        // display: 'block';
+        nav.style.display = 'block';
+    } else {
+        nav.animate([
+            {
+                top: '59px',
+                opacity: 1
+            },
+            {
+                top: '0',
+                opacity: 0
+            },
+        ], {
+            duration: 300,
+            fill: 'forwards'
+        });
+        // display: 'none';
+        setTimeout(() => {
+            nav.style.display = 'none';
+        }, 300);
+    }
+    isopen = !isopen;
+});
